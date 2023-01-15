afcc32fe08b0917b3c7d97607217488520e6f51d
Seungjae Park <astro.psj@gmail.com>
2023-01-15 21:02:11 +0900

    Add lecture 1 resource

---
+++ lect1/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "quiz.h"
+
+int main() {
+    Student kim("김**", 100);
+    Student lee("이**", 90);
+    Student park("박**", 80);
+    Professor choi("최**", "정보통신공학과");
+
+    Lecture cpp("C++ 심화", choi, { kim, lee });
+
+    std::cout << cpp.get_name() << " 성적\n";
+    for (const Student &student : cpp.get_students()) {
+        std::cout << student.get_name() << ": " << student.get_grade() << '\n';
+    }
+
+    cpp.add_student(park);
+    std::cout << '\n';
+
+    std::cout << cpp.get_name() << " 성적\n";
+    for (const Student &student : cpp.get_students()) {
+        std::cout << student.get_name() << ": " << student.get_grade() << '\n';
+    }
+
+    return 0;
+}
---
+++ lect1/quiz.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Person {
+public:
+    explicit Person(const std::string &name);
+    virtual ~Person() = default;
+    virtual const std::string &get_name() const;
+
+private:
+    std::string name;
+};
+
+class Professor : public Person {
+public:
+    Professor(const std::string &name, const std::string &department_name);
+    const std::string &get_department_name() const;
+
+private:
+    std::string department_name; // 소속 학과
+};
+
+class Student : public Person {
+public:
+    Student(const std::string &name, int grade);
+    int get_grade() const;
+
+private:
+    int grade; // 1학년~4학년
+};
+
+class Lecture {
+public:
+    Lecture(const std::string &name, const Professor &professor, const std::vector<Student> &students);
+    void add_student(const Student &student);
+    const std::string &get_name() const;
+    const Professor &get_professor() const;
+    const std::vector<Student> &get_students() const;
+
+private:
+    std::string name;
+    Professor professor;
+    std::vector<Student> students;
+};
