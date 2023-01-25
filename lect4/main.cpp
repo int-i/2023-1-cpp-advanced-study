0c9ec777c676d325da9e809659b147dd8b350c89
Seungjae Park <astro.psj@gmail.com>
2023-01-25 17:46:00 +0900

    Add lect4 resource

--- lect4/main.cpp
+++ lect4/main.cpp
@@ -2,6 +2,7 @@
 #include <mutex>
 #include <semaphore>
 #include <thread>
+#include "quiz.h"
 
 // std::mutex mutex;
 std::counting_semaphore<1> mutex(1);
@@ -42,15 +43,44 @@ void sum(int a, int b) {
     }
 }
 
+MutexQueue<int> mq;
+// std::queue<int> mq;
+
+void push_numbers() {
+    for (int n = 0; n < 100; n += 1) {
+        mq.push(n);
+    }
+}
+
+void pop_numbers() {
+    for (int n = 0; n < 50; n += 1) {
+        mq.pop();
+    }
+}
+
 int main() {
-    std::thread t1(sum, 0, 25);
-    std::thread t2(sum, 25, 50);
-    std::thread t3(sum, 50, 75);
-    std::thread t4(sum, 75, 101);
+    // std::thread t1(sum, 0, 25);
+    // std::thread t2(sum, 25, 50);
+    // std::thread t3(sum, 50, 75);
+    // std::thread t4(sum, 75, 101);
+    // t1.join();
+    // t2.join();
+    // t3.join();
+    // t4.join();
+    // std::cout << N;
+
+    std::thread t1(push_numbers);
+    std::thread t2(push_numbers);
     t1.join();
     t2.join();
+    std::cout << "size: " << mq.size() << '\n'; // 200
+    
+    std::thread t3(pop_numbers);
+    std::thread t4(pop_numbers);
+    std::thread t5(pop_numbers);
     t3.join();
     t4.join();
-    std::cout << N;
+    t5.join();
+    std::cout << "size: " << mq.size() << '\n'; // 50
     return 0;
 }
---
+++ lect4/quiz.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <mutex>
+#include <queue>
+
+template<typename T>
+class MutexQueue {
+public:
+    MutexQueue() = default;
+
+    explicit MutexQueue(std::queue<T> queue)
+    : queue(queue) {}
+
+    int size() const {
+        return queue.size();
+    }
+
+    T pop() {
+        // 
+    }
+
+    void push(T value) {
+        std::lock_guard<std::mutex> lock(mutex);
+        queue.push(value);
+    }
+
+private:
+    std::mutex mutex;
+    std::queue<T> queue;
+};
